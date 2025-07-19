7460d3455ff07346c0f2ef6ab8e0eef2d0b504fe
Istiak Mahmud <istiakiraz@gmail.com>
2025-07-19 17:42:22 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className='bg-base-100 shadow-sm'>
            <div className="navbar md:w-10/12 mx-auto w-11/12 ">
   {/* <div className="flex-none">
-    <button className="btn  btn-ghost">
+    <button className="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
     </button>
   </div> */}
